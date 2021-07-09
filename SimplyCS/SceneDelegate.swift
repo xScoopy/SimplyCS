4cc8cbb0354a0aa7497df8384a775ab98b97f0c3
xScoopy <JLeary070@gmail.com>
2021-07-09 18:58:15 -0500

    Project init

--- SimplyCS/SceneDelegate.swift
+++ SimplyCS/SceneDelegate.swift
@@ -13,10 +13,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        // Create an instance of the main view controller and a navigation controller
+        let mainController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainController)
+
+        // Tell the window to load the main controller as it's root view
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
